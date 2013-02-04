af360f41fd68299fb0dce2a9ec0705cfe46b7a21
kurik <honza.kurik@gmail.com>
2013-02-04 12:50:13 +0100

    Baselining

--- yaws/src/unin.erl
+++ yaws/src/unin.erl
@@ -1,6 +1,28 @@
 -module('unin').
 -export([out/1]).
 
-out(_Arg) ->
-	io:format("Hello from Unin~n"),
-	{html, "<p>Hello from Unin<p>"}.
+%%% -include("/usr/lib/yaws/include/yaws_api.hrl").
+-include("/home/kurik/opt/yaws/lib/yaws/include/yaws_api.hrl").
+
+out(Arg) ->
+	Method = method(Arg),
+	Resource = resource(Arg),
+	% io:format("Resource: ~p~n", [Resource]),
+	handle(Method, Resource, Arg).
+
+method(Arg) ->
+	Rec = Arg#arg.req,
+	Rec#http_request.method.
+
+resource(Arg) ->
+	Resource = string:sub_word(Arg#arg.pathinfo, 1, $/),
+	Resource.
+
+handle(Method, Resource, Arg) ->
+	case Resource of
+		"temp" ->
+			temp:handle(Method, Arg);
+		_ ->
+			{status, 404}
+	end.
+
