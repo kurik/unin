1cdee6c8e9382e992f3d33b3b2e7cbb1310dab7e
Jan Kurik <honza.kurik@gmail.com>
2013-03-27 10:21:02 +0100

    Fix of broken config separation

--- google_sheet.py
+++ google_sheet.py
@@ -5,11 +5,11 @@ import datetime
 import w1_term
 import my_config
 
-gc = gspread.login(google_login, google_password)
-wks = gc.open(google_sheet_name).sheet1
+gc = gspread.login(my_config.google_login, my_config.google_password)
+wks = gc.open(my_config.google_sheet_name).sheet1
 
 now = datetime.datetime.now()
 
-wks.update_acell('B2', float(w1_term.Therm(out_sensor).getDegrees()) / 1000)
-wks.update_acell('C2', float(w1_term.Therm(in_sensor).getDegrees()) / 1000)
+wks.update_acell('B2', float(w1_term.Therm(my_config.out_sensor).getDegrees()) / 1000)
+wks.update_acell('C2', float(w1_term.Therm(my_config.in_sensor).getDegrees()) / 1000)
 wks.update_acell('D2', now.strftime("%Y-%m-%d\n%H:%M"))
