ea09d8b18139fdf46447d60bc678eeaf2d2891dd
Jan Kurik <honza.kurik@gmail.com>
2013-03-27 10:13:57 +0100

    Publishing of temperature in Google spreadsheet

---
+++ my_config.py
@@ -0,0 +1,12 @@
+# A configuration file for thermo mesurement plaing
+
+# Name of the Google spreadsheet
+google_sheet_name = 'Temperature'
+# Google account (email address)
+google_login = 'myemail@gmail.com'
+# The big secret comes here
+google_password = 'nereknu'
+
+# Sensors (indoor one and outdoor one)
+out_sensor = '28-000001b4337c'
+in_sensor = '28-000001b4754f'
