08c3382d7f94b6044486aefe2fd19dcda610828a
Jan Kurik <jan.kurik@gooddata.com>
2013-04-03 10:56:46 +0200

    Added prototypes of modules

---
+++ csvstore.py
@@ -0,0 +1,4 @@
+import os
+
+def main(queues):
+		print('Inside the module', __name__, ':: PID = ', os.getpid())
--- module.py
+++ module.py
@@ -1,10 +1,13 @@
+import importlib
+
 class Module:
 		def __init__(self, name, config):
 				self.name = name
 				self.config = config
 				self.process = None
+				self.m = importlib.import_module(name)
 				print('Module name:', name, 'created')
 
 		def start(self, queues):
 				self.queues = queues
-				print('Module', self.name, 'running')
+				self.m.main(queues)
