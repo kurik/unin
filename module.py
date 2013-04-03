ea30a9e8e04d66b693fd988c85053fbf59ff3799
Jan Kurik <jan.kurik@gooddata.com>
2013-04-03 11:16:15 +0200

    Move of modules into a separate directory

--- module.py
+++ module.py
@@ -1,3 +1,4 @@
+import os, sys, inspect
 import importlib
 
 class Module:
@@ -5,8 +6,12 @@ class Module:
 				self.name = name
 				self.config = config
 				self.process = None
+				# Make sure the directory of modules is in our modules' path
+				cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"modules")))
+				if cmd_subfolder not in sys.path:
+						sys.path.insert(0, cmd_subfolder)
+				# Import the module
 				self.m = importlib.import_module(name)
-				print('Module name:', name, 'created')
 
 		def start(self, queues):
 				self.queues = queues
