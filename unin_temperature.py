5000d8fc461cbb79d84ae03ffac270b06fe33ab6
Jan Kurik <jkurik@redhat.com>
2013-09-22 21:27:42 +0200

    Initial implementation of asynchronous store of temperatures to DB and GSheet

--- unin_temperature.py
+++ unin_temperature.py
@@ -8,6 +8,7 @@ import os
 import optparse
 import gspread
 import datetime
+from multiprocessing import Pool
 
 
 CONFIG_FILE="~/etc/unin_temperature.conf"
@@ -36,41 +37,51 @@ for sensor in sensors:
 
 
 # Save temperature to SQLite
-if options.verbose:
-    print('Going to save temperature into DB ... ',)
-with sqlite3.connect(os.path.expanduser(config['DEFAULT']['sqlitedb'])) as db:
-    sql = db.cursor()
-    # Make sure we have all tables defined
-    sql.execute("CREATE TABLE IF NOT EXISTS sensor(sensorid TEXT UNIQUE, note TEXT)")
-    sql.execute("CREATE TABLE IF NOT EXISTS temperature(stamp TEXT DEFAULT (datetime('now')), temperature REAL, sensor INTEGER)")
+def save_to_db(config, sensors, temperatures):
+    if options.verbose:
+        print('Going to save temperature into DB ... ',)
+    with sqlite3.connect(os.path.expanduser(config['DEFAULT']['sqlitedb'])) as db:
+        sql = db.cursor()
+        # Make sure we have all tables defined
+        sql.execute("CREATE TABLE IF NOT EXISTS sensor(sensorid TEXT UNIQUE, note TEXT)")
+        sql.execute("CREATE TABLE IF NOT EXISTS temperature(stamp TEXT DEFAULT (datetime('now')), temperature REAL, sensor INTEGER)")
 
-    # Go throught all we have from sensors
-    for sensor in sensors:
-        # Do we have the sensor in DB ?
-        sql.execute("SELECT oid FROM sensor WHERE sensorid = ?", (sensor,))
-        oid = sql.fetchone()
-        if oid is None:
-            # We need to insert the sensor into DB
-            sql.execute("INSERT INTO sensor(sensorid) VALUES(?)", (sensor,))
-            # Get the OID
-            oid = sql.lastrowid
-        else:
-            oid = oid[0]
-        # Save the temperature
-        sql.execute("INSERT INTO temperature(sensor, temperature) VALUES(?,?)", (oid, temperatures[sensor]))
+        # Go throught all we have from sensors
+        for sensor in sensors:
+            # Do we have the sensor in DB ?
+            sql.execute("SELECT oid FROM sensor WHERE sensorid = ?", (sensor,))
+            oid = sql.fetchone()
+            if oid is None:
+                # We need to insert the sensor into DB
+                sql.execute("INSERT INTO sensor(sensorid) VALUES(?)", (sensor,))
+                # Get the OID
+                oid = sql.lastrowid
+            else:
+                oid = oid[0]
+            # Save the temperature
+            sql.execute("INSERT INTO temperature(sensor, temperature) VALUES(?,?)", (oid, temperatures[sensor]))
 
-if options.verbose:
-    print('DONE')
+    if options.verbose:
+        print('DONE')
 
 # Save the temperature to Google spreadsheet
-if options.verbose:
-    print('Saving temperature to Google spreadsheet ... ',)
-gc = gspread.login(config['GSHEET']['user'], config['GSHEET']['password'])
-spreadsheet = gc.open(config['GSHEET']['sheet_name'])
-w_summary = spreadsheet.worksheet('SUMMARY')
-now = datetime.datetime.now()
-w_summary.update_acell('B2', float(temperatures[config['GSHEET']['out_sensor']]) / 1000)
-w_summary.update_acell('C2', float(temperatures[config['GSHEET']['in_sensor']]) / 1000)
-w_summary.update_acell('A1', now.strftime("%Y-%m-%d\n%H:%M"))
-if options.verbose:
-    print('DONE')
+def save_to_gsheet((config, sensors, temperatures)):
+    if options.verbose:
+        print('Saving temperature to Google spreadsheet ... ',)
+    gc = gspread.login(config['GSHEET']['user'], config['GSHEET']['password'])
+    spreadsheet = gc.open(config['GSHEET']['sheet_name'])
+    w_summary = spreadsheet.worksheet('SUMMARY')
+    now = datetime.datetime.now()
+    w_summary.update_acell('B2', float(temperatures[config['GSHEET']['out_sensor']]) / 1000)
+    w_summary.update_acell('C2', float(temperatures[config['GSHEET']['in_sensor']]) / 1000)
+    w_summary.update_acell('A1', now.strftime("%Y-%m-%d\n%H:%M"))
+    if options.verbose:
+        print('DONE')
+
+# Save to DB and GSpreadsheet
+pool = Pool(processes=2)
+db_result = pool.apply_async(save_to_db, (config, sensors, temperatures))
+gs_result = pool.apply_async(save_to_gsheet, (config, sensors, temperatures))
+db_result.get(timeout=15)
+gs_result.get(timeout=30)
+
