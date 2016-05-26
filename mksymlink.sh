3e334ebdb040deffa250fbf68b32f9dc24379b5a
Jan Kurik <honza.kurik@gmail.com>
2016-05-26 13:46:23 +0200

    Create hard links in temp_upload users home directory

--- mksymlink.sh
+++ mksymlink.sh
@@ -4,5 +4,14 @@ function get_db() {
     /store/devel/unin.devel/uninconfig.py | awk '/Database filename: / {print $3}'
 }
 
+function updatelinks() {
+    if cd ~temp_upload/var; then
+        for i in /root/var/unin_temperature.db*; do
+            [[ -e $(basename ${i}) ]] || ln $i
+        done
+    fi
+}
+
 rm -f /root/var/unin_temperature.db
 ln -s "$(get_db)" /root/var/unin_temperature.db
+updatelinks
