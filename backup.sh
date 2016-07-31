f609977d355593947a5dd4530b0ef93223995e65
Jan Kurik <honza.kurik@gmail.com>
2016-07-31 07:56:47 +0200

    Fixed incorred calculation of the previous month in backup.sh

--- backup.sh
+++ backup.sh
@@ -4,15 +4,25 @@ USER="temp_measure"
 GROUP="temperature"
 DIR="/home/temp_measure/var"
 DB="unin_temperature.db"
-TSTAMP=$(date +'%Y-%m' -d "1 month ago")
+
+function backupday {
+    date +'%Y %m' | (\
+        read Y M;\
+        if [[ $M -eq 1 ]]; then\
+            printf "%04d-%02d" $(( $Y - 1 )) 12 ;\
+        else\
+            printf "%04d-%02d" $Y $(( $M - 1 )) ;\
+        fi;\
+    )
+}
 
 # Check whether there is anything to do
-if [[ -e "${DIR}/${DB}.${TSTAMP}" ]]; then
+if [[ -e "${DIR}/${DB}.$(backupday)" ]]; then
     # The file seems to exists
     :
 else
     # The backup file does not exists, create it
-    mv "${DIR}/${DB}" "${DIR}/${DB}.${TSTAMP}"
+    mv "${DIR}/${DB}" "${DIR}/${DB}.$(backupday)"
     touch "${DIR}/${DB}"
     chmod 0640 "${DIR}/${DB}"
     chown ${USER}:${GROUP} "${DIR}/${DB}"
